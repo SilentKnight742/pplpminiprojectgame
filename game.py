5b59439e5eaa0158c0b7b131638882d465f448f0
BiJu-02 <67651024+BiJu-02@users.noreply.github.com>
2021-10-14 09:44:31 +0530

    Update game.py

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from cards import *
 class Player:
     def __init__(self):
         self.username = 'dio da'
+        self.clicks = 0
         # player ke stats kya kya hone chayiye decide krna hai ;-;
     
     # yaha pe game functionalities
