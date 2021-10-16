173bb66114054d3e751f11f25e6f68e4052c6dec
BiJu-02 <67651024+BiJu-02@users.noreply.github.com>
2021-10-17 01:43:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-import kivy 
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
-from kivy.core.audio import SoundLoader
-from kivy.clock import Clock
-from game import *
 import pickle
+
+import kivy
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
+from kivy.uix.screenmanager import ScreenManager
+
+from game import *
+
 
 class groot(ScreenManager):
     def __init__(self,**kwargs):
-        super(groot,self).__init__(**kwargs)     
-        # yeh commented out stuff was sockets wala part...abhi chodo yeh
-        # self.n = Client()
-        # self.other_client = info() 
-        # self.my = self.n.getP()
+        super(groot,self).__init__(**kwargs)  
 
         # player object initiation
         self.player = Player()
@@ -20,8 +19,21 @@ class groot(ScreenManager):
         # loading all the required sounds
         self.clik_snd = SoundLoader.load('resc/sounds/click.wav')
 
+        # temp var for traversing list of langs of player
+        self.tempID = 0
+
+        # temp var for traversing list of all skills
+        self.tempID2 = 0
+
+        # temp var for checking new card
+        self.new = True
+        self.newID = 0
+
+        # end of root construtor
+
+
         
-        
+    # to check if the player_data file already exists or not
     def acc(self):
         try:
             player_data = open('player_data.bin','rb')
@@ -32,30 +44,140 @@ class groot(ScreenManager):
             pass
             # print(';-;')
         return False
-        
+
+        # end of acc func
+
+
+
+    # the update func is for updating time, player_data file and some info displayed on screen
     def starto(self):
         Clock.schedule_interval(self.update,0.0167)
         # basically setting the fps ...60 fps ~ 1 frame  / 0.0167 ms ...i think i right...hoefully
-        # more like we'll keep updating the file where we store player data and the data we show on screen around 60 times in a sec...yeh number ke saath thoda experiment krke dekhenge
-        # kabhi koi abruptly app close bhi kr diya toh stats lose nhi hoga...we'll pickle the player object and store it in a .bin file
-        # demn itna comments kabhi nhi likha...
+        # more like we'll keep updating the file where we store player data and the data we show on screen around 60 times in a sec...
+      
+        # end of starto func
+
+
 
     # dt is a special variable by kivy thats passed to the callback funcs initiated by schedule interval func...apne kaam ka nhi...
-    def update(self,dt):                             # use this to update every value on screen ;-;
-        # self.ids.id_of_widget.widget_attribute   ...for acessing the widget attributes in kv file
-        # self.ids.butt1.text = self.player.username + ' ' + str(self.player.clicks)
+    def update(self,dt):                             
+        # use this to update every value on screen ;-;
+
+        # updating time for quiz and daily rewards
+        t = time.time()
+        if self.player.qz_time_left <= 1:
+            pass
+            self.ids.quiz_butt.disabled = False                                   
+            self.ids.quiz_butt.text = 'Quiz'
+        else:
+            self.player.qz_time_left -= (t - self.player.qz_last_time_rec)
+            self.player.qz_last_time_rec = t
+            self.ids.quiz_butt.disabled = True
+            self.ids.quiz_butt.text = str(math.floor(self.player.qz_time_left))
+
+        if self.player.dly_time_left <=0:
+            self.ids.dly_butt.disabled = False                                   
+        else:
+            self.player.dly_time_left -= (t - self.player.dly_last_time_rec)
+            self.player.dly_last_time_rec = t
+            self.ids.dly_butt.disabled = True
+
+ 
+        # updating game screen
+        if gr.current == 'game_screen':
+            
+            # lang card view
+            if len(self.player.langs)!=0:
+                k = self.player.langs[self.player.lang_ind[self.tempID]]
+                self.ids.game_lang_card_img.source = 'resc/images/' + k.img_name
+                self.ids.game_lang_card_stat.text = 'Name: ' + k.name + '\nLevel: ' + str(k.card_lvl) + '\nExp: +' + str(k.card_exp)
+                if self.tempID==0:
+                    self.ids.game_prev_butt.disabled = True
+                else:
+                    self.ids.game_prev_butt.disabled = False
+                if self.tempID==len(self.player.langs)-1:
+                    self.ids.game_next_butt.disabled = True
+                else:
+                    self.ids.game_next_butt.disabled = False
+            else:
+                self.ids.game_prev_butt.disabled = True
+                self.ids.game_next_butt.disabled = True
+                self.ids.game_lang_card_img.source = 'resc/images/no_card.png' 
+                self.ids.game_lang_card_stat.text = 'Try rolling a new card'
+            
+            # roll butt
+            if self.player.energy < 300:
+                self.ids.roll_butt.disabled = True
+            else:
+                self.ids.roll_butt.disabled = False
+
+            # player stat
+            self.ids.play_stat_window.text = '[b][size=25]' + self.player.username + '[/size][/b]' + '\n\nLevel: ' + str(self.player.lvl) + '\nExp: ' +  str(self.player.exp) + '\nEnergy: ' + str(self.player.energy) + '\nProficiency: ' + str(self.player.pp) + '\nFame: x' + str(self.player.fame)                                                
+            
+
+        # updating skill screen
+        if gr.current == 'skill_screen':
+            k = skill_lst[self.tempID2]
+            self.ids.skill_card_img.source = 'resc/images/' + k.img_name
+            self.ids.skill_card_stat.text = 'Name: ' + k.name + '\nFame: ' + str(k.skill_fame) + '\nCost: ' + str(k.cost)
+            if self.tempID2==0:
+                self.ids.skill_prev_butt.disabled = True
+            else:
+                self.ids.skill_prev_butt.disabled = False
+            if self.tempID2==len(skill_lst)-1:
+                self.ids.skill_next_butt.disabled = True
+            else: 
+                self.ids.skill_next_butt.disabled = False
+            if self.player.skill_exists(self.tempID2) or self.player.pp<k.cost:
+                self.ids.buy_skill_butt.disabled = True
+            else:
+                self.ids.buy_skill_butt.disabled = False
+
+        
+        # updating rolled screen
+        if gr.current == 'rolled_card_screen':
+            if not self.new:
+                self.ids.burn_butt.disabled = False
+                self.ids.keep_butt.disabled = False
+                self.ids.rolled_to_game.disabled = True
+                self.ids.rolled_card_label.text = 'You already have this card'
+            else:
+                self.ids.keep_butt.disabled = True
+                self.ids.burn_butt.disabled = True
+                self.ids.rolled_to_game.disabled = False
+                self.ids.rolled_card_label.text = 'Yay! you got a new card'
+
+
+
+
+        # updating player_data file
         try:
             player_data = open('player_data.bin','wb')
             pickle.dump(self.player,player_data)
             player_data.close()
         except:
             print('missed')
-        self.ids.butt2.text = self.player.username
 
+        
+
+
+
+        # end of update func
+        
     
+
+    def dly_reset(self):
+        self.player.dly_time_left = self.player.dly_cd
+        self.player.dly_last_time_rec = time.time()
+    
+    def qz_reset(self):
+        self.player.qz_time_left = self.player.qz_cd
+        self.player.qz_last_time_rec = time.time()
 
 
     def clicked(self):
+        if self.clik_snd:
+            self.clik_snd.play()
         # self.player.clicks = (self.player.clicks+1)%20
         # if self.player.clicks%2==0:
         #     self.ids.butt1.background_normal = 'resc/images/groot.png'
@@ -63,8 +185,15 @@ class groot(ScreenManager):
         # else:
         #     self.ids.butt1.background_normal = 'resc/images/dio.png'
         #     self.ids.butt1.text = 'dio da'
-        if self.clik_snd:
-            self.clik_snd.play()
+
+    def roll(self):
+        self.newId = self.player.rng()
+        if self.player.lang_exists(self.newId):
+            self.new = False
+        else:
+            self.new = True
+            self.player.add(self.newId)
+
 
 
 
@@ -72,7 +201,9 @@ class groot(ScreenManager):
 
 class main(App):
     def build(self):
-        return groot()
+        global gr
+        gr = groot()
+        return gr
 
 if __name__ == '__main__':
     main().run()
