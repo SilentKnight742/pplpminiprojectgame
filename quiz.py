6640198c8eb9bbb888fb63827476ff39bd6bc736
BiJu-02 <67651024+BiJu-02@users.noreply.github.com>
2021-10-17 14:12:51 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,11 @@
+
+class quest:
+    def __init__(self,que,opt,ans):
+        self.ques = que
+        self.opt = opt
+        self.ans = ans
+
+que_lst = [
+    quest('the que',['opt1','opt2','opt3','opt4'],2)      # example   
+    # and so on
+]
