c0a5e83f9ce54b0da00d143e92d1703e66b4e490
silentknight <chitnisapoorv@gmail.com>
2021-10-18 20:08:17 +0530

    40% complete

--- quiz.py
+++ quiz.py
@@ -15,5 +15,15 @@ que_lst = [
     quest('Which of the following is not a high programming language',['Assembly','C++','Java','Python'],1),
     quest('Resolving errors in a program is known as',['Problem Solving','Refixing','Error Checking','Debugging'],4),
     quest('A language that is close to human language and which is very easy to write, debug and maintain is known as...',['Machine Code','Low Level Language','High Level Language','Algorithm'],3),
-    quest('Which of the following command would not be found in an assembly language',['LOAD','STORE','ADD','SORT'],4)
+    quest('Which of the following command would not be found in an assembly language',['LOAD','STORE','ADD','SORT'],4),
+    quest('What is the name for the software used to convert an assembly language program into machine code?',['Translator','Interpreter','Compiler','Assembler'],4),
+    quest('Which of the following is not an advantage of using a low level language?',['Makes more efficient use of primary memory.','To enable the program to run on multiple platforms','To perform a task very quickly','Tailor a program to a specific piece of hardware'],2),
+    quest('The two categories of low level language are...',['Machine Code and Assembly','Machine Code and Algorithms','Assembly and Algorithms','Algorithms and Binary'],1),
+    quest('The high-level programming language that was developed in the mid-1970s that is now used to write applications for nearly every available platform is...',['Scratch','Bark-Bark','Tynker','C++'],4),
+    quest('Software that translates and executes a high level language program one line at a time is known as a?',['Compiler','Assembler','Interpreter','Executor'],3),
+    quest('Which type of translator creates an executable file of machine code from a program written in a high level language?',['Assembler','Compiler','Interpreter','Executor'],2),
+    quest('The 3 main types of translators are...',['Assemblers, Compilers and Interpreters','Assemblers, Compilers and Converters','Assemblers, Scripters and Interpreters','Converters, Scripters and Interpreters'],1),
+    quest('Assembly language is...',['A small business meeting','A high level language like java','Used for manufacturing automobiles','a low level machine languge'],4),
+    quest('What is the smallest unit of measurement used to quantify computer data?',['a birdle','a marble','a bit','a tablespoon'],3),
+    quest('What is an open source, high-level programming language designed to be easy to read and simple to implement?',['Sunmatra','Java','Blocks','Latin'],2)
 ]
